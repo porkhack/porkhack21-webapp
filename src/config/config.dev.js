45731e2df35e3c6692f32221a2822aa3259c3b28
Aaron Neustedter <aneusted@purdue.edu>
2021-03-25 21:49:48 -0400

    Login broken

--- src/config/config.dev.js
+++ src/config/config.dev.js
@@ -1,6 +1,6 @@
 //Local Development Values
 const devtoolsPort = 8787;
-const oadaDomain = process.env.REACT_APP_OADA_DOMAIN || "https://localhost";
+const oadaDomain = process.env.REACT_APP_OADA_DOMAIN || "http://localhost";
 const websiteDomain = "http://localhost:" + parseInt(window.location.port, 10);
 //const websiteDomain = 'http://vip3.ecn.purdue.edu:'+parseInt(window.location.port, 10);
 const metadata = require("./dev_metadata.js");
