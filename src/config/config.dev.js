34212ee78a9ac90da64d34d11bf29147c72c6894
Aaron Neustedter <aneusted@purdue.edu>
2021-03-25 22:24:07 -0400

    New dev server

--- src/config/config.dev.js
+++ src/config/config.dev.js
@@ -4,7 +4,7 @@ const oadaDomain = process.env.REACT_APP_OADA_DOMAIN || "https://localhost";
 const websiteDomain = "https://localhost:" + parseInt(window.location.port, 10);
 //const websiteDomain = 'http://vip3.ecn.purdue.edu:'+parseInt(window.location.port, 10);
 const metadata = require("./dev_metadata.js");
-const defaultNewConnectionURL = "https://farmer.porkhack1.openag.io";
+const defaultNewConnectionURL = "https://farmer.porkhack2.openag.io";
 
 export default {
   oadaDomain,
