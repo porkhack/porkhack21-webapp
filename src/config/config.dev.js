3d30cffb63bd35f14d0e5347e0964cf06a491db5
Aaron Neustedter <aneusted@purdue.edu>
2021-03-25 22:15:13 -0400

    These are self signed development certs, don't get so excited

--- src/config/config.dev.js
+++ src/config/config.dev.js
@@ -1,7 +1,7 @@
 //Local Development Values
 const devtoolsPort = 8787;
-const oadaDomain = process.env.REACT_APP_OADA_DOMAIN || "http://localhost";
-const websiteDomain = "http://localhost:" + parseInt(window.location.port, 10);
+const oadaDomain = process.env.REACT_APP_OADA_DOMAIN || "https://localhost";
+const websiteDomain = "https://localhost:" + parseInt(window.location.port, 10);
 //const websiteDomain = 'http://vip3.ecn.purdue.edu:'+parseInt(window.location.port, 10);
 const metadata = require("./dev_metadata.js");
 const defaultNewConnectionURL = "https://farmer.porkhack1.openag.io";
